7cd9309ed79fd4706fe1230693147d9a929fe303
fish895623 <dan990429@gmail.com>
2022-07-18 11:11:57 +0900

    Testing Grab

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,6 +1,14 @@
 #!/usr/bin/env groovy
+@Grab('org.apache.commons:commons-math3:3.4.1')
+import org.apache.commons.math3.primes.Primes
+
+void parallelize(int count) {
+  if (!Primes.isPrime(count)) {
+    error "${count} was not prime"
+  }
+}
 
 def call(String name = 'human') {
   echo "Hello, ${name}."
+  parallelize(3)
 }
-
