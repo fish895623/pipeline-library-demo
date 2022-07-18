1f0d77f2dd4c6c583c6c268a0f0b48b0df8b7308
fish895623 <dan990429@gmail.com>
2022-07-18 11:39:06 +0900

    Import Local

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -3,5 +3,15 @@
 import sayBye
 
 def call(String agent = 'any') {
-  sayBye()
+  agent {
+    dockerfile {
+      filename '.docker/a.Dockerfile'
+        args '--user 0:984'
+        reuseNode true
+    }
+  }
+  steps {
+    // sh 'python3 test/test_main.py'
+    sayHello 'asdf'
+  }
 }
